933d0b5bb2f458936179f39c0c5ef59821055d89
cmartineau15 <charlotte.martineau@epitech.digital>
2025-09-12 12:35:33 +0200

    Add databse connection

---
+++ BDD/Connection_BDD.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine, text
+
+# Remplace les infos avec tes identifiants réels
+engine = create_engine("postgresql+psycopg2://postgres:1504@localhost:5432/sneakers")
+
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT now()"))
+    for row in result:
+        print(row)
